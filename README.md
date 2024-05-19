b90e11ca5a6a84bf29b3ff7f762cfef6816c6efd
Vindyani1999 <k.a.c.h.vindyani@gmail.com>
2024-05-19 07:57:53 +0530

    settup server port and mogodb connection

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Chat-App
+
 MERN Stack Project
---
+++ server/index.js
@@ -0,0 +1,36 @@
+//Getting express web framework from nodejs
+const express = require("express");
+
+//Getting cors to handle backend server requests from frontend origin
+const cors = require("cors");
+
+//Getting mongoose to manage relationship between data and the scema
+const mongoose = require("mongoose");
+
+//creating app using express framework
+const app = express();
+
+//Configering .env file
+require("dotenv").config();
+
+//using middleware
+app.use(cors());
+app.use(express.json());
+
+//creating mongodb connection
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB has been connected!");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+//creating server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server Started on port ${process.env.PORT}`);
+});
